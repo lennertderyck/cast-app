fdc84eab1a17e215686c36beb02c5be3633e5c0c
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-08-16 23:35:15 +0200

    Update

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 620,
+    height: 320,
     title: 'Cast App',
     titleBarStyle: 'hiddenInset',
     alwaysOnTop: true,
--- preload.js
+++ preload.js
@@ -1,7 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const path = require('path');
-const find = require('find');
 const fs = require('fs');
 const {node, eventCallback, getFormData, Element} = require('cutleryjs/dist/js/legacy.min.js');
 const {sesamCollapse, sesam} = require('sesam-collapse/dist/legacy.min.js')
@@ -9,19 +8,44 @@ const ChromecastAPI = require('chromecast-api');
 const client = new ChromecastAPI();
 const movieInfo = require('movie-info');
 
+const browserSync = require("browser-sync");
+
+browserSync({  
+    port: 9001,
+    server: '/',
+    open: false,
+    ui: {
+        port: 8080
+    },
+    callbacks: {
+        ready: (err, bs) => {
+            console.log(bs);
+        }
+    }
+});
+
 window.addEventListener('DOMContentLoaded', async () => {
+    window.appSettings = {};
+    
     app.init();
     
     await client.on('device', (device) => {
         devices.load(device);
     })
-    console.log('loaded')
 })
+
+client.on('device', function (device) {
+    var mediaURL = 'file://Users/lennertderyck/Movies/een berichtje voor de welpies.mp4';
+  
+    device.play(mediaURL, function (err) {
+      if (!err) console.log('Playing in your chromecast')
+    })
+  })
 
 const app = {
     init() {
         app.listeners();
-        
+        network.init();
         sesamCollapse.initialize();
     },
     
@@ -45,20 +69,23 @@ const devices = {
         // devices.displayList();
     },
     
-    play() {
+    play(url = window.appSettings.loadedFile.path) {
+        console.log(url);
+        const backupUrl = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
         const selectedDevice = devices.getSelected();
         
-        var mediaURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
-       
-        selectedDevice.play(mediaURL, (err) => {
+        selectedDevice.play(url, (err) => {
           if (!err) console.log('Playing in your chromecast')
           if (err) console.log(err);
         })
     },
     
+    getFileFromList() {
+    
+    },
+    
     setPlaying() {
-        // edit json data and dom element
-        // save dom element in json
+        // edit playing node by json data
     },
     
     set(name) {       
@@ -102,8 +129,6 @@ const devices = {
 
 const files = {
     async add(file) {
-        console.log(file);
-        
         const dir = path.dirname(file.path);
         const thumb = await files.findThumb(dir);
         
@@ -111,10 +136,20 @@ const files = {
         file.thumb = `${dir}/${thumb}`
         file.node = files.render(file);
         
+        files.setToPlay(file);
         filesList.set(file.path, file);
         files.checkExisting(); // hide fileuploader if one or more files are added
+    },
+    
+    setToPlay(file) {
+        const $fileToPlay = file.node.return();
+        const $fileToStop = node('[data-section="filesList"] .list .state__playing')
         
-        // devices.play();
+        if ($fileToStop) $fileToStop.setAttribute('data-state', 'notplaying');
+        $fileToPlay.setAttribute('data-state', 'playing');
+        window.appSettings.loadedFile = file;
+        
+        devices.play();
     },
     
     async findThumb(dirname) {
@@ -175,3 +210,44 @@ const files = {
         })
     }
 }, filesList = new Map();
+
+const network = {
+    init() {
+        network.render();
+        network.constantTesting();
+    },
+    
+    getSpeed() {
+        const online = window.navigator.onLine;
+        const speed =  online ? navigator.connection.effectiveType : online;
+        return {
+            false: 0,
+            'slow-2g': 1,
+            '2g': 1,
+            '3g': 2,
+            '4g': 3,
+        }[speed];
+    },
+    
+    constantTesting() {
+        console.log('constantTesting');
+        
+        navigator.connection.addEventListener('change', (event) => {
+            const speed = network.getSpeed(event.target.effectiveType);
+            network.render(speed);
+        });
+    },
+    
+    render(speed = network.getSpeed()) {
+        const text = {
+            0: 'offline',
+            1: 'slow',
+            2: 'good',
+            3: 'strong'
+        }[speed]
+        const $networkSpeed = node('[data-label="networkSpeed"]');
+        const $networkSpeedText = node('[data-label="networkSpeed"] span')
+        $networkSpeed.setAttribute('data-networkspeed', text);
+        $networkSpeedText.innerHTML = text;
+    }
+}
